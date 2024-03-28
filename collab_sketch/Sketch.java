b772c43177010f51f5891168a137e205885e02a1
yanghwa2001 <162687179+yanghwa2001@users.noreply.github.com>
2024-03-28 15:06:11 +0900

    Add files via upload

---
+++ collab_sketch/Sketch.java
@@ -0,0 +1,86 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Sketch Class, PS6
+ * @Author Ryan Lee, CS10, Winter 2021
+ */
+
+public class Sketch {
+    private TreeMap<Integer, Shape> shapeMap; //Map that contains shapes
+    static int shapeID; //id of shapes
+
+    public Sketch(){ //instantiates Sketch, starting shapeID at 0 and creating new treemap for shapeMap
+        shapeID = 0;
+        this.shapeMap = new TreeMap<>();
+    }
+
+    public Map<Integer, Shape> getShapeMap() {
+        return this.shapeMap;
+    } //get function for shapeMap
+
+    public int getShapeId(Point p){ //gets ID for shape
+        for (int id : shapeMap.descendingKeySet()) {
+            if (shapeMap.get(id).contains(p.x, p.y)) {
+                return id; //if the shape is identified, return the id of that shape
+            }
+        }
+        return 0; //if the specified shape is not found, return 0
+    }
+
+    public Shape getShape(int id){
+        return shapeMap.getOrDefault(id, null);
+    } //get function for a specific shape
+
+    public synchronized void addShape(Shape shape) {    //add function for shapes
+        shapeMap.put(shapeID, shape);
+        shapeID++;
+    }
+
+    public synchronized void removeShape(Shape shape) { //remove function for shape
+        for(Integer id : shapeMap.descendingKeySet()){
+            if(shapeMap.get(id).equals(shape)){ //find that shape, remove it
+                shapeMap.remove(id);
+                return;
+            }
+        }
+    }
+
+    public ArrayList<Shape> shapeList() {   //creates list of shapes
+        ArrayList<Shape> list = new ArrayList<>();
+        for(Integer id : shapeMap.navigableKeySet()){
+            list.add(shapeMap.get(id));
+        }
+        return list;
+    }
+
+    public synchronized void clear(){ //clear function for the board
+        shapeMap.clear();
+        shapeID = 0;
+    }
+
+    public synchronized void setShapeID(int id, Shape shape){
+        shapeMap.put(id, shape);
+    }  //sets an id for a shape
+
+    public synchronized void recolorShape(int id, Color color) {
+        shapeMap.get(id).setColor(color);
+    } //recoloring method
+
+    public synchronized void moveShape(int id, int dx, int dy) {
+        shapeMap.get(id).moveBy(dx, dy);
+    }  //moves the shape
+
+    @Override
+    public String toString(){ //toString method for all the shapes in shapeMap
+        String s = "";
+        for(Integer id : shapeMap.descendingKeySet()){
+            s += id + "," + shapeMap.get(id).toString() + " ";
+        }
+        return s;
+    }
+
+}
---
+++ collab_sketch/SketchServerCommunicator.java
@@ -0,0 +1,156 @@
+import java.awt.*;
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Handles communication between the server and one client, for SketchServer
+ *
+ * @author Chris Bailey-Kellogg, Dartmouth CS 10, Fall 2012; revised Winter 2014 to separate SketchServerCommunicator
+ */
+public class SketchServerCommunicator extends Thread {
+	private Socket sock;					// to talk with client
+	private BufferedReader in;				// from client
+	private PrintWriter out;				// to client
+	private SketchServer server;			// handling communication for
+
+	public SketchServerCommunicator(Socket sock, SketchServer server) {
+		this.sock = sock;
+		this.server = server;
+	}
+
+	/**
+	 * Sends a message to the client
+	 * @param msg
+	 */
+	public void send(String msg) {
+		out.println(msg);
+	}
+	
+	/**
+	 * Keeps listening for and handling (your code) messages from the client
+	 */
+	public void run() {
+		try {
+			System.out.println("someone connected");
+			
+			// Communication channel
+			in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			out = new PrintWriter(sock.getOutputStream(), true);
+
+			// Tell the client the current state of the world
+			// TODO: YOUR CODE HERE
+			send(server.getSketch().toString());
+
+			// Keep getting and handling messages from the client
+			// TODO: YOUR CODE HERE
+			String line;
+			while (((line = in.readLine()) != null)) {
+				readLineHelper(line);
+			}
+
+			// Clean up -- note that also remove self from server's list so it doesn't broadcast here
+			server.removeCommunicator(this);
+			out.close();
+			in.close();
+			sock.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void readLineHelper(String str) {
+		String[] split = str.split(","); // finds what the command is
+		if (split[0] == "add") {
+			handleAddingShape(split);
+		}
+		if (split[0] == "delete") {
+			handleDeletingShape(split);
+		}
+		if (split[0] == "recolour") {
+			handleRecoloringShape(split);
+		}
+		if (split[0] == "move") {
+			handleMovingShape(split);
+		}
+	}
+
+	public void handleAddingShape(String[] strings) {
+		System.out.println("ADD mode!");
+		Shape shape = null;
+		String s = strings[1];
+		if (s.equals("ellipse") || s.equals("rectangle") || s.equals("segment")) {
+			int x1 = Integer.parseInt(strings[2]);
+			int y1 = Integer.parseInt(strings[3]);
+			int x2 = Integer.parseInt(strings[4]);
+			int y2 = Integer.parseInt(strings[5]);
+			int rgb = Integer.parseInt(strings[6]);
+			if (s == "ellipse") {
+				Color c = new Color(rgb);
+				shape = new Ellipse(x1, y1, x2, y2, c);
+			}
+			if (s == "rectangle") {
+				Color c = new Color(rgb);
+				shape = new Rectangle(x1, y1, x2, y2, c);
+			}
+			if (s == "segment") {
+				Color c = new Color(rgb);
+				shape = new Segment(x1, y1, x2, y2, c);
+			}
+		} else {
+			int n = (strings.length - 3) / 2;
+			int[] xPoints = new int[n];
+			int[] yPoints = new int[n];
+			int j = 2;
+			while (j < strings.length - 1) {
+				int x = Integer.parseInt(strings[j]);
+				xPoints[(j - 2) / 2] = x;
+				int y = Integer.parseInt(strings[j + 1]);
+				yPoints[(j - 2) / 2] = y;
+				j += 2;
+			}
+			int rgb = Integer.parseInt((strings[(strings.length) - 1]));
+			Color c = new Color(rgb);
+			shape = new Polyline(xPoints, yPoints, c);
+		}
+		server.getSketch().setShapeID(SketchServer.id, shape);
+		server.broadcast("adding " + SketchServer.id + ", " +
+				server.getSketch().getShapeMap().get(SketchServer.id).toString());
+		SketchServer.id++;
+	}
+
+	public void handleDeletingShape(String[] strings) {
+		System.out.println("delete mode");
+		int id = Integer.parseInt(strings[1]);
+		Shape shape = server.getSketch().getShapeMap().get(id);
+		if (shape != null) {
+			server.getSketch().getShapeMap().remove(id);
+			server.broadcast("delete " + id);
+		}
+	}
+
+	public void handleRecoloringShape(String[] strings) {
+		System.out.println("recolouring");
+		int id = Integer.parseInt(strings[1]);
+		int rgb = Integer.parseInt(strings[2]);
+		Shape shape = server.getSketch().getShapeMap().get(id);
+		if (shape != null) {
+			shape.setColor(new Color(rgb));
+			server.getSketch().setShapeID(id, shape);
+			server.broadcast("recolour, " + id + "," + shape.getColor().getRGB());
+		}
+	}
+
+	public void handleMovingShape(String[] strings) {
+		System.out.println("move mode");
+		int id = Integer.parseInt(strings[1]);
+		int dx = Integer.parseInt(strings[2]);
+		int dy = Integer.parseInt(strings[3]);
+		Shape shape = server.getSketch().getShapeMap().get(id);
+		if (shape != null) {
+			shape.moveBy(dx, dy);
+			server.getSketch().setShapeID(id, shape);
+			server.broadcast("move," + id + "," + dx + "," + dy);
+		}
+	}
+}
